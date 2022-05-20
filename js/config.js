60f00dac130f54ed9539013d0164f1fbaf34a938
kamil.klimaszewski@gmail.com <kamil.klimaszewski@gmail.com>
2022-05-20 14:27:37 +0200

    config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_3S6jeIrxV', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1a2knm85vbp5q2oh86n0gqci4c', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
